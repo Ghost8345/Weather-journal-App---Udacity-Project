c3c4ea93bfa015b7d71f4a23541a53d566be7c3f
Meniem Hany <es-abdelmoneim.hany2019@alexu.edu.eg>
2022-07-24 18:52:55 +0200

    All functionalites done

---
+++ website/app.js
@@ -0,0 +1,79 @@
+/* Global Variables */
+
+const server = "http://localhost:8080";
+
+const baseURL = "http://api.openweathermap.org/data/2.5/weather?zip=";
+
+// Use metric units ie. Celsius
+const apiKey = "&appId=61caa58492750f6277fcc316dbb99cde&units=metric";
+
+// Create a new date instance dynamically with JS
+let d = new Date();
+let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+
+const getWeather = async (zip) => {
+    const res = await fetch(baseURL + zip + apiKey);
+    try {
+        const data = await res.json();
+        if(data.cod != 200){
+            alert("Please Enter valid Zip Code");
+            throw error;
+        }
+        console.log("Get:");
+        console.log(data);
+        return data;
+    }
+    catch(error) {
+        console.log(error);
+    }
+};
+
+const postData = async (url = "", data = {}) => {
+    const req = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json;charset=UTF-8"
+      },
+      body: JSON.stringify(data),
+    });
+  
+    try {
+      const newData = await req.json();
+      console.log(" Post: ");
+      console.log(newData);
+      return newData;
+    }
+    catch (error) {
+      console.log(error);
+    }
+  };
+
+  const updateUI = async () => {
+    const request = await fetch(server + "/all");
+    try {
+        const data = await request.json();
+        console.log("Update: ")
+        console.log(request);
+        console.log(data);
+        document.getElementById("date").innerHTML = data.date;
+        document.getElementById("temp").innerHTML = data.temp;
+        document.getElementById("content").innerHTML = data.content;
+        document.getElementById("location").innerHTML = data.location;
+    }
+    catch (error) {
+        console.log("error", error);
+    }
+};
+
+const generate = (e) => {
+    e.preventDefault();
+    const zip = document.querySelector("#zip").value;
+    const content = document.querySelector("#feelings").value;
+    getWeather(zip)
+    .then( (data) => {
+        postData(server + "/generate", {date: newDate, temp: data.main.temp.toString() + " °C", content: content, location: data.name})
+        .then( (newData) => updateUI())
+    })
+}
+
+document.getElementById("generate").addEventListener("click", generate);
